f940dab2887b7657d6eb11e853e979a4080c343d
youjin <yjinhann@gmail.com>
2023-05-18 10:32:45 +0900

    feat: 필터별로 todo 추가하기

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { theme } from './colors';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const YourApp = () => {
   const [doing, setDoing] = useState(true);
   const [text, setText] = useState('');
+  const [todos, setTodos] = useState({});
   const task = () => setDoing(false);
   const todo = () => setDoing(true);
   const onChangeText = (payload) => setText(payload);
-
+  const addToDo = () => {
+    if (text === '') return;
+    const newToDos = { ...todos, [Date.now()]: { text, work: doing } };
+    setTodos(newToDos);
+    setText('');
+    // savetodo
+  };
+  console.log(todos);
   return (
     <View style={styles.container}>
       <StatusBar style='auto' />
@@ -22,11 +31,22 @@ const YourApp = () => {
         </TouchableOpacity>
       </View>
       <TextInput
+        onSubmitEditing={addToDo}
         onChangeText={onChangeText}
+        returnKeyType='done'
         value={text}
         placeholder={doing ? 'Add a To Do' : 'Add a Task'}
         style={styles.input}
       />
+      <ScrollView>
+        {Object.keys(todos).map((key) =>
+          todos[key].work === doing ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{todos[key].text}</Text>
+            </View>
+          ) : null
+        )}
+      </ScrollView>
     </View>
   );
 };
@@ -54,7 +74,19 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 20,
     borderRadius: 30,
-    marginTop: 20,
+    marginVertical: 20,
     fontSize: 18,
+  },
+  toDo: {
+    backgroundColor: theme.grey,
+    marginBottom: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+  },
+  toDoText: {
+    color: theme.white,
+    fontSize: 16,
+    fontWeight: '500',
   },
 });
